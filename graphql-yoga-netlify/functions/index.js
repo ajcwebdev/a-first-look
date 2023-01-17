d5b67e8778abca198dd032dc7f213f06c67c6a03
Anthony Campolo <admin@ajcwebdev.com>
2023-01-17 02:15:43 -0600

    remove graphql examples

--- graphql-yoga-netlify/functions/index.js
+++
@@ -1,7 +0,0 @@
-const { GraphQLServerLambda } = require('graphql-yoga')
-
-const typeDefs = `type Query { hello: String }`
-const resolvers = { Query: { hello: () => 'Hello from GraphQL Yoga on Netlify!' } }
-const lambda = new GraphQLServerLambda({ typeDefs, resolvers })
-
-exports.handler = lambda.handler
