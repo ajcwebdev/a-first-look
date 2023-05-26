3ec199082ae4d70e23687747a35eccfe63786df1
Anthony Campolo <12433465+ajcwebdev@users.noreply.github.com>
2023-05-26 12:34:06 -0500

    react18

--- frontend/react18/src/main.jsx
+++ frontend/react18/src/main.jsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import App from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
